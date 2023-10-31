ec465760c15630d1489e8f7dbe60e3735b14d89e
yusuf-ishaku <kingshakzy@gmail.com>
2023-10-31 20:22:32 +0100

    WAhala

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,6 @@
 import type React from "react"
 
-export default function Layout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
+export default function Layout() {
     return (
       <section>
             News page
