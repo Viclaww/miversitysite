e4dd78e45cea267d81857c83b9a5b40781fd0f48
Your Name <youremail@example.com>
2023-12-03 23:01:31 +0100

    Added Password Toggle

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -2,12 +2,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import Mask from "../../images/Mask group.png";
-
-export default function signin() {
+import { useState } from "react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+export default function Signin() {
+  const [visible, setVisible] = useState(false);
+  const togglePasswordVisibility = () => {
+    setVisible((prev) => !prev);
+  };
   return (
     <>
       <section className="w-[90vw] md:w-[80vw]   flex justify-between items-center mx-auto h-[70vh] p-10 rounded-xl my-16  bg-auth ">
-        <div className="w-full md:w-3/5">
+        <div className="w-full ">
           <div className="login-text">
             <h3 className="text-3xl my-2">Log Into Your Account</h3>
             <p className="text-lg">
@@ -19,12 +24,27 @@ export default function signin() {
               Registration Number
             </label>
             <input className=" auth-input" type="text" />
-            <label className="text-sm my-1 text-[#0A3E5C]" htmlFor="">
-              Password
-            </label>
-            <input className="auth-input" type="password" />
+            <div className="flex flex-1 relative flex-col">
+              <label className="text-sm my-1 text-[#0A3E5C]" htmlFor="">
+                Password
+              </label>
+              <span className="absolute right-2 md:right-24 cursor-pointer top-9">
+                {!visible ? (
+                  <IoMdEye onClick={togglePasswordVisibility} size={28} />
+                ) : (
+                  <IoMdEyeOff onClick={togglePasswordVisibility} size={28} />
+                )}
+              </span>
+              <input
+                id="password"
+                className="auth-input"
+                type={visible ? "text" : "password"}
+              />
+            </div>
 
-            <button></button>
+            <button className="bg-[#FFA800] py-2 rounded-lg md:w-4/5 hover:bg-[#EEA000]">
+              Sign In
+            </button>
             <p>
               Don’t have an account?{" "}
               <Link href={"/"} className="text-[#2894F8]">
